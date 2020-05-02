c37db956bd7ff6e6a9b843ad1798531a9b3b6ffe
Adrian Lee <adrianlee0118@gmail.com>
2020-05-01 21:12:11 -0700

    edited js files, added python

--- static/dice_function.js
+++ static/dice_function.js
@@ -1,6 +1,40 @@
 var index = Math.floor(Math.random() * diceRolls.length); //start at a random place in the array
-var maxDiceRoll = 32
+var maxDiceRoll = 32;
+var result1 = "-----";
+var result2 = "";
 
+//Retrieves parameters, increases index and retrieves value from pre-generated random sequence, calculates roll(s)
+function roll(dicesize) {
+  load()
+  var times = document.getElementById("times").value;
+  var bonus = document.getElementById("bonus").value;
+  var btype = "bonus";
+  if (bonus < 0){
+    btype = "handicap";
+  }
+
+  var res = [];
+  for (i = 0; i < times; i++){
+    increaseIndex();
+    var normalizedRoll = Math.ceil(((diceRolls[index] + 1)/maxDiceRoll)*dicesize);
+    res.push(normalizedRoll+bonus);
+  }
+  result1 = `You rolled a ${dicesize}-sided die ${times} times with a ${bonus} ${btype}`
+  result2 = 
+  
+  document.getElementById("result1").innerHTML = result;
+}
+
+//Get the answer area to display a loading gif for 1 second before displaying the answer
+function load() {
+  document.getElementById("result1").style.display = "none";
+  document.getElementById("loader").style.display = "block";
+
+  setTimeout(function() {
+    document.getElementById("result1").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+  }, 1000);
+}
 
 function increaseIndex() { //go to the next number. If its the end of the array, go back to the front
   if (index < diceRolls.length-1) {
@@ -8,61 +42,4 @@ function increaseIndex() { //go to the next number. If its the end of the array,
   } else {
     index = 0;
   }
-}
-
-var result = "---"
-var finalDiceSize = "---";
-
-// Make all die except the passed diceID faded
-function fadeDice(diceID) {
-  for (var i = 1; i <= 6; i++) {
-      var item = document.getElementById(i);
-      item.classList.remove("inactive");
-  }
-
-  for (var i = 1; i <= 6; i++) {
-    if(i != diceID) {
-      var item = document.getElementById(i);
-      item.className += " inactive";
-    }
-  }
-}
-
-//Get dice size from argument, set to the global variable
-function getDiceSize(diceSize) {
-  finalDiceSize = diceSize;
-  return finalDiceSize;
-}
-
-// When a dice button is clicked, fade all dice except for clicked dice, set dice size
-function rollDice(diceID, diceSize) {
-  fadeDice(diceID);
-  getDiceSize(diceSize);
-}
-
-//Get the loader bar at the bottom of the screne to display answer after one second
-function load() {
-  document.getElementById("result").style.display = "none";
-  document.getElementById("loader").style.display = "block";
-
-  setTimeout(function() {
-    document.getElementById("result").style.display = "block";
-    document.getElementById("loader").style.display = "none";
-  }, 1000);
-}
-
-//Retrieves parameters, increases index and retrieves value from pre-generated random sequence, calculates roll
-function roll() {
-  load()
-  if (Number.isNaN(result)) {
-    result = "---";
-  }
-  if (finalDiceSize == "---") {
-    result = "---";
-  } else {
-    increaseIndex();
-    var normalizedRoll = (diceRolls[index] + 1)/maxDiceRoll;
-    result = Math.ceil(normalizedRoll * finalDiceSize);
-  }
-  document.getElementById("result").innerHTML = result;
 }
